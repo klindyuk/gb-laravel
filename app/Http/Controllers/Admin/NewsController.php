3ad84f8a0727ed4f213ddd5931c1c3388547c5a2
klindyuk <klindyuk@gmail.com>
2020-08-19 23:55:25 +0300

    hw6+

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -3,33 +3,105 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\NewsStoreRequest;
+use App\Models\News;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
-class NewsController extends \App\Http\Controllers\Controller
+class NewsController extends Controller
 {
-    public function create() {
-        return view('admin.news.create', ['categories' => $this->categories]);
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     */
+    public function index()
+    {
+        $newsList = News::paginate(10);
+        return view('admin.news.index', ['newsList' => $newsList]);
     }
-    public function edit(int $id) {
-        $key = array_search($id, array_column($this->news, 'id'));
-        $item = $this->news[$key];
-        return view('admin.news.edit', ['item' => $item, 'categories' => $this->categories]);
-    }
-    public function store(NewsStoreRequest $request) {
-        $data = $request->only(['id', 'category_id', 'title', 'text']);
-        $saveFile = function(array $data) {
-            $responseData = [];
-            $fileNews = storage_path('app/news.txt');
-            if (file_exists($fileNews)) {
-                $file = file_get_contents($fileNews);
-                $responseData = json_decode($file, true);
-            }
-            $responseData[] = $data;
-            file_put_contents($fileNews, json_encode($responseData, JSON_UNESCAPED_UNICODE));
-        };
 
-        $saveFile($data);
-        return redirect()->route('index');
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('admin.news.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->only('title', 'slug', 'img', 'text');
+        if (!$data['slug']) {
+            $data['slug'] = Str::slug($data['title'], '-');
+        }
+        $news = News::create($data);
+        if ($news) {
+            return redirect()->route('news.index')->with('success', 'Новость добавлена');
+        }
+        return back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\News  $news
+     * @return \Illuminate\Http\Response
+     */
+    public function show(News $news)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\News  $news
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     */
+    public function edit(News $news)
+    {
+        return view('admin.news.edit', ['news' => $news]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\News  $news
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, News $news)
+    {
+        $news->title = $request->input('title');
+        $news->slug = $request->input('slug');
+        $news->img = $request->input('img');
+        $news->text = $request->input('text');
+
+        if ($news->save()) {
+            return redirect()->route('news.index')->with('success', 'Новость обновлена');
+        }
+        return back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\News  $news
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(News $news)
+    {
+        if ($news->delete()) {
+            return redirect()->route('news.index')->with('success', 'Новость удалена');
+        }
+        return back();
     }
 }
--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,23 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function index()
-    {
-        return view('category/index');
-    }
-
-    public function show(int $id)
-    {
-//        $key = array_search($id, array_column($this->categories, 'id'));
-//        $category = $this->categories[$key];
-//        $news = array_filter($this->news, function($v) use ($id) {return $v['category_id'] == $id; });
-        $category = (new \App\Models\Category())->getById($id);
-//        $news = (new \App\Models\News())->getAll();
-        $news = (new \App\Models\News())->getByCategoryId($id);
-        return view('category/show', ['category' => $category, 'news' => $news]);
+    public function show(Category $category) {
+        return view('category.show', ['category' => $category]);
     }
 }
